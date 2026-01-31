c63395b7560f68a2947bbfc77dfee41111a6b5b1
FrancuzA <carquier@edu.cdv.pl>
2026-01-31 01:44:01 +0100

    trees

--- Assets/Scripts/InteractableObjects/TreeScript.cs
+++ Assets/Scripts/InteractableObjects/TreeScript.cs
@@ -1,22 +1,44 @@
+using System;
 using UnityEngine;
 
 public class TreeScript : MonoBehaviour, IInteractable
 {
     private int treeHP = 100;
     private TreeStump treeStump;
+    private float treeSize;
+
+    private void Start()
+    {
+        if (Physics.Raycast(gameObject.transform.position, Vector3.down, out RaycastHit hitInfo, treeSize/2))
+        {
+            if (hitInfo.collider.gameObject.TryGetComponent(out TreeStump stump))
+            {
+                treeStump= stump;
+            }
+        }
+        else
+        {
+            Debug.Log("no stump found");
+        }
+    }
+
     public void Interact()
     {
-        
+        DamageTree();
     }
 
     private void DamageTree()
     {
         treeHP -= 10;
-        
+        if (treeHP <= 0)
+        {
+            InformStump();
+            Destroy(gameObject);
+        }
     }
 
     private void InformStump()
     {
-        
+        treeStump.StartGrowingTree();
     }
 }
--- Assets/Scripts/InteractableObjects/TreeStump.cs
+++ Assets/Scripts/InteractableObjects/TreeStump.cs
@@ -1,16 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TreeStump : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float timer = 50;
+    public GameObject treeBody;
+    public Vector3 treeSpawnOffset;
+
+
+    public void CreateNewTree()
     {
-        
+        Instantiate(treeBody, transform.position + treeSpawnOffset, Quaternion.identity, gameObject.transform);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGrowingTree()
     {
-        
+        StartCoroutine(GrowTree());
+    }
+
+    public IEnumerator GrowTree()
+    {
+        yield return new WaitForSecondsRealtime(timer);
+        CreateNewTree();
     }
 }
